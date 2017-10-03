299f2cef56702597011e712da60eda613fff91fc
HuaBa <liuhualin333@gmail.com>
2017-10-03 09:58:28 +0800

    add token dist graph and move code to specific folder

--- dataCollection/dbStatistics.py
+++ dataCollection/dbStatistics.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 import matplotlib.pyplot as plt
+import nltk.data
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -16,14 +18,13 @@ def create_connection(db_file):
  
     return None
 
-db_file = 'posts/2008Posts.db' 
+db_file = '../posts/2008Posts.db' 
 db_conn = create_connection(db_file)
 cursor = db_conn.cursor()
 
 #Sample testing code
 cursor.execute('''SELECT answercount, count(id)  FROM posts group by answercount''')
 r = cursor.fetchall()
-print(tuple(r))
 
 plt.figure()
 x = [ele[0] for ele in list(r)]
@@ -37,11 +38,9 @@ plt.show()
 
 cursor.execute('''SELECT count(id) FROM posts''')
 p = cursor.fetchone()
-print(tuple(p))
 
 cursor.execute('''SELECT count(id) FROM answers''')
 a = cursor.fetchone()
-print(tuple(a))
 
 plt.figure()
 labels = ['posts','answers']
@@ -53,5 +52,28 @@ for x, y in zip(x, y):
 plt.xlabel('Categories')
 plt.ylabel('Number of posts/answers')
 plt.title('Count of answers/posts')
-plt.savefig('Answers_Posts(Count).png')
+plt.savefig('answersPostsCount.png')
+plt.show()
+
+cursor.execute('''SELECT body FROM posts''')
+t = cursor.fetchall()
+tokenCount = {}
+for body in t:
+    body = body[0].encode('ascii','ignore')
+    print(body)
+    sentences = tokenizer.tokenize(body)
+    tokenNum = 0
+    for sentence in sentences:
+        tokenNum += len(nltk.word_tokenize(sentence))
+        try:    
+            tokenCount[tokenNum] += 1
+        except:
+            tokenCount[tokenNum] = 1
+plt.figure()
+for k,v in tokenCount.items():
+    plt.bar(k, v,color='darkblue')
+plt.xlabel('Number of token in a post')
+plt.ylabel('Number of posts')
+plt.title('Count of tokens in the post')
+plt.savefig('postTokenCount.png')
 plt.show()
