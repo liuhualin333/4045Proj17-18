3aa0d87f8467f728f5c32c29a8212a601eba23b1
Chen-Hailin <chenhailin2@gmail.com>
2017-10-20 15:18:03 +0800

    chl

---
+++ tokenizer/run.sh
@@ -0,0 +1,5 @@
+python code_tokenizer.py ../posts/answers_training_clean.txt   
+python text_tokenizer.py ../posts/answers_training_clean_codeAnno.txt
+python code_tokenizer.py ../posts/posts_training_clean.txt
+python text_tokenizer.py ../posts/posts_training_clean_codeAnno.txt
+echo 'finish'
