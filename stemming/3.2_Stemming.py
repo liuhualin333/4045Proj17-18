9755033ae70f6ee0149d4e617507bbc9589c15a3
Ziji Shi <shi.ziji.sm@gmail.com>
2017-10-23 22:15:54 +0800

    - Stemming and pos-tagging using NLTK

--- stemming/3.2_Stemming.py
+++ stemming/3.2_Stemming.py
@@ -2,38 +2,38 @@
 # Use an off-the-shelf tool to stem the tokens.
 # Based on the comparison from nltk.org, snowball stemming is preferred over porter stemming for higher accuracy
 from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.porter import PorterStemmer
-from nltk import tokenize
+from nltk.tokenize import word_tokenize
 from collections import Counter
+
+def removeDuplicate(ls):
+    newList = []
+    for elt in ls:
+        if elt not in newList:
+            newList.append(elt)
+    return newList
 
 # Create an English stemmer insatnce
 stemmer = SnowballStemmer("english")
-stemmer2= PorterStemmer()
 
-# TODO : Get a list of tokens from dataset and sort in order of occurrence
-myfile = open("/Users/StevenShi/PycharmProjects/NTUNLP/posts/answers_training_clean.txt")
-inreader = myfile.read().decode('utf8').encode('utf-8')
-myfile.close()
-
-tokens = []
-for line in inreader:
-    tokens = tokens+ tokenize(line)
-
+myfile = open("/Users/StevenShi/PycharmProjects/NTUNLP/posts/answers_training_clean.txt", encoding="utf-8")
+# Attention here : inreader is a single string
+inreader = myfile.read()
+tokens = word_tokenize(inreader)
 # tokens = ['caresses', 'sized','sized','sized','flies', 'dies', 'mules', 'denied','died', 'agreed', 'owned', 'flies','humbled', 'sized','meeting', 'stating', 'siezing', 'itemization','sensational', 'traditional', 'reference', 'colonizer','plotted']
+
 sortedTokens =sorted(tokens, key=Counter(tokens).get, reverse=True)
 
 print(tokens)
-print(sortedTokens)
+print(removeDuplicate(sortedTokens))
 
 # TODO : Stemming the tokens
 stemmedTokens = []
-porterStemmed = []
+
 for token in tokens:
     stemmedTokens.append(stemmer.stem(token))
-    porterStemmed.append(stemmer2.stem(token))
 
 # Compare results
 print(stemmedTokens)
-print(porterStemmed)
 
 
+myfile.close()
