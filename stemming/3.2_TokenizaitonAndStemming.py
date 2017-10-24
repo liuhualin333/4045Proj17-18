3cdf6d4dd288ff979cae700c4775d161e6ec5972
Ziji Shi <shi.ziji.sm@gmail.com>
2017-10-24 12:45:55 +0800

    - provided sample output for off-the-shelf tokenizer and stemmer

--- stemming/3.2_TokenizaitonAndStemming.py
+++ stemming/3.2_TokenizaitonAndStemming.py
@@ -6,7 +6,6 @@ import sys
 import re
 from io import StringIO
 
-import os
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
 from collections import Counter
@@ -18,8 +17,8 @@ class StringBuilder:
      def __init__(self):
          self._file_str = StringIO()
 
-     def Append(self, str):
-         self._file_str.write(str)
+     def Append(self, Str):
+         self._file_str.write(Str)
 
      def __str__(self):
          return self._file_str.getvalue()
@@ -44,7 +43,6 @@ def main(file):
     sortedTokens_file=StringBuilder()
     reducedSortedTokens_file=StringBuilder()
     # file_anchor mark the current position in current file, before which has been processed already
-    file_anchor = 0
     # Skip the first line
     anchor_answer = re.compile(re.escape('Id|Body')).search(source, 0)
     anchor_post = re.compile(re.escape('Id|Title|Body')).search(source, 0)
@@ -54,11 +52,8 @@ def main(file):
         file_anchor = anchor_post.end()
     else:
         file_anchor = 0
-    sb_file = StringBuilder()
-    sb_file.Append(source[0:file_anchor])
     # Define text as "not code"
     for code_sec in code_secs:
-        print("there")
         code_start = code_sec.start()
         code_end = code_sec.end()
         text = source[file_anchor:code_start]
@@ -79,7 +74,7 @@ def main(file):
 
         file_anchor = code_end
 
-    # Create annotated file
+    # Write to output
     with open("Tokens.txt", "w+") as t_file:
         t_file.write(tokens_file.__str__())
     with open("SortedTokens.txt","w+") as s_file:
