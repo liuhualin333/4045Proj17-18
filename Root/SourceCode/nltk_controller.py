50e7af057d2dc50978f879a40f2c628c3f539c5b
Ziji Shi <shi.ziji.sm@gmail.com>
2017-11-02 22:42:42 +0800

    Corrected nltk_controller.py

--- Root/SourceCode/nltk_controller.py
+++ Root/SourceCode/nltk_controller.py
@@ -2,23 +2,24 @@
 This is the controller for 3.2 Stemming and POS-tagging and 3.4 Further Analysis.
 '''
 import sys
+import sklearn_crfsuite
 import os.path
 sys.path.insert(0, '../')
 sys.path.insert(0, '../Data')
 sys.path.insert(0, './Stemming_and_POStagging')
 sys.path.insert(0, './utilities')
 import utilities
-import Root.SourceCode.Stemming_and_POStagging.Tokenization_And_Stemming as ts
-import Root.SourceCode.tokenizer as nlp
-import Root.SourceCode.Stemming_and_POStagging.Analyser as analyser
+import SourceCode.Stemming_and_POStagging.Tokenization_And_Stemming as ts
+import SourceCode.tokenizer as nlp
+import SourceCode.Stemming_and_POStagging.Analyser as analyser
 
 
 def doNLTK():
     def doStem():
         print()
         print(">>> It may take a few minutes to finish processing :)")
-        stemmed_tkn_list = ts.main("./Data/All_Posts.txt",True)
-        unstemmed_tkn_list = ts.main("./Data/All_Posts.txt", False)
+        stemmed_tkn_list = ts.main("../Data/All_Posts.txt",True)
+        unstemmed_tkn_list = ts.main("../Data/All_Posts.txt", False)
         # stemmed_tkn_list=[('>', 93016), ('<', 92745), ("''", 52246), ('p', 23261), ('/p', 23249), (',', 18417), (':', 15211), ('.', 12136), ('pre', 11362), ('/pre', 11357), ('use', 6380), ('i', 5953), (')', 5609), ('python', 5575), ('(', 5390), ('/a', 4050), ('href=', 4050), ('rel=', 3046), ("'s", 2688), ('noreferr', 2606)]
         # unstemmed_tkn_list=[('>', 93016), ('<', 92745), ("''", 52246), ('p', 23252), ('/p', 23249), (',', 18417), (':', 15211), ('.', 12136), ('pre', 11362), ('/pre', 11357), ('I', 5953), (')', 5609), ('(', 5390), ('Python', 4067), ('href=', 4050), ('/a', 4050), ('use', 3081), ('rel=', 3046), ("'s", 2688), ('noreferrer', 2606)]
 
@@ -32,7 +33,7 @@ def doNLTK():
     def doPOSTag():
         from nltk import word_tokenize, pos_tag
 
-        with open("./Data/Pos-Tag-source.txt","r") as infile:
+        with open("../Data/Pos-Tag-source.txt","r") as infile:
             content = infile.readlines()
             sentenseList = [x.strip('\n') for x in content]
             for sentense in sentenseList:
@@ -52,11 +53,11 @@ def doIrregularCheck():
     print("followed by selecting 10 existing sentences that contains the irregular tokens.")
 
     def doTokenize():
-        nlp.main("./Data/all_posts_clean.txt")
+        nlp.main("../Data/all_posts_clean.txt")
         print("Question tokenization finished.")
-        nlp.main("./Data/all_answers_clean.txt")
+        nlp.main("../Data/all_answers_clean.txt")
         print("Answers tokenization finished.")
-        myTokens= analyser.Analyser("./Data/all_posts_clean_predict_tokenized.txt","./Data/all_answers_clean_predict_tokenized.txt")
+        myTokens= analyser.Analyser("../Data/all_posts_clean_predict_tokenized.txt","./Data/all_answers_clean_predict_tokenized.txt")
         tops=int(input("How many tokens do you want to see? [Suggested number : 50 ]"))
         myTokens.doRanking(top=tops)
     doTokenize()
